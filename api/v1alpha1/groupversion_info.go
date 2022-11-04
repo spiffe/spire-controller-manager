4c3ae75d40dfc0f1c028b48a19b993b7880871b5
Andrew Harding <aharding@vmware.com>
2022-11-04 16:30:16 -0600

    Fix status subresource in demo
    The demo configuration was not updated to include updates to the CRD
    YAML after the status subresource was added. This causes the status
    update to fail when running an environment based on the demo.
    
    Fixes: #33
    
    Signed-off-by: Andrew Harding <aharding@vmware.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the spire v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=spire.spiffe.io
+// +kubebuilder:object:generate=true
+// +groupName=spire.spiffe.io
 package v1alpha1
 
 import (
