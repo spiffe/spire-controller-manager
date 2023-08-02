f7510b043f8409e584139f6958f31b2a86690d38
Marcos Yacob <marcos.yacob@hpe.com>
2023-08-02 15:24:32 -0300

    Filter unsupported fields (#185)
    * Filter unsupported fields when reconcile entries is called
    
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>
    Co-authored-by: Andrew Harding <azdagron@gmail.com>

--- pkg/spireapi/entryapi.go
+++ pkg/spireapi/entryapi.go
@@ -18,17 +18,34 @@ package spireapi
 
 import (
 	"context"
+	"fmt"
 
 	entryv1 "github.com/spiffe/spire-api-sdk/proto/spire/api/server/entry/v1"
+	"github.com/spiffe/spire-api-sdk/proto/spire/api/types"
 	apitypes "github.com/spiffe/spire-api-sdk/proto/spire/api/types"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
+
+const (
+	AdminField         Field = "admin"
+	DNSNamesField      Field = "dnsNames"
+	DownstreamField    Field = "downstream"
+	FederatesWithField Field = "federatesWith"
+	HintField          Field = "hint"
+	JWTSVIDTTLField    Field = "jwtSVIDTTL"
+	X509SVIDTTL        Field = "x509SVIDTTL"
+)
+
+type Field string
 
 type EntryClient interface {
 	ListEntries(ctx context.Context) ([]Entry, error)
 	CreateEntries(ctx context.Context, entries []Entry) ([]Status, error)
 	UpdateEntries(ctx context.Context, entries []Entry) ([]Status, error)
 	DeleteEntries(ctx context.Context, entryIDs []string) ([]Status, error)
+	GetUnsupportedFields(ctx context.Context, td string) (map[Field]struct{}, error)
 }
 
 func NewEntryClient(conn grpc.ClientConnInterface) EntryClient {
@@ -57,6 +74,65 @@ func (c entryClient) ListEntries(ctx context.Context) ([]Entry, error) {
 		}
 	}
 	return entriesFromAPI(entries)
+}
+
+func (c entryClient) GetUnsupportedFields(ctx context.Context, td string) (map[Field]struct{}, error) {
+	resp, err := c.api.BatchCreateEntry(ctx, &entryv1.BatchCreateEntryRequest{
+		Entries: []*apitypes.Entry{
+			{
+				ParentId: &types.SPIFFEID{
+					TrustDomain: td,
+					Path:        "/spire-controller-manager/unsupported-fields-test",
+				},
+				SpiffeId: &types.SPIFFEID{
+					TrustDomain: td,
+					Path:        "/spire-controller-manager/unsupported-fields-test",
+				},
+				Selectors: []*types.Selector{
+					{
+						Type:  "a",
+						Value: "1",
+					},
+				},
+				X509SvidTtl: 60,
+				JwtSvidTtl:  60,
+				Hint:        "hint",
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(resp.Results) != 1 {
+		return nil, fmt.Errorf("only one response expected but got %v", len(resp.Results))
+	}
+
+	result := resp.Results[0]
+	if result.Status.Code != int32(codes.OK) {
+		return nil, fmt.Errorf("failed to create entry: %v", result.Status.Message)
+	}
+
+	_, err = c.api.BatchDeleteEntry(ctx, &entryv1.BatchDeleteEntryRequest{
+		Ids: []string{
+			result.Entry.Id,
+		},
+	})
+	if err != nil {
+		log := log.FromContext(ctx)
+		log.Error(err, "failed to delete dummy entry", "entry_id", result.Entry.Id)
+	}
+
+	unsupportedFields := make(map[Field]struct{})
+	if result.Entry.JwtSvidTtl == 0 {
+		unsupportedFields[JWTSVIDTTLField] = struct{}{}
+	}
+
+	if result.Entry.Hint == "" {
+		unsupportedFields[HintField] = struct{}{}
+	}
+
+	return unsupportedFields, nil
 }
 
 func (c entryClient) CreateEntries(ctx context.Context, entries []Entry) ([]Status, error) {
