977a4ed421529e35fcf74334c5a302f01c12ec32
pratik-lotia <pratik.lotia@reddit.com>
2025-04-09 17:43:00 -0400

    nit: fix typo in demo script
    Signed-off-by: pratik-lotia <pratik.lotia@reddit.com>

--- demo/scripts/show-spire-federated-bundles.sh
+++ demo/scripts/show-spire-federated-bundles.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 kubectl exec -t \
-    -nspire-system \
+    -n spire-system \
     -c spire-server deployment/spire-server -- \
         /opt/spire/bin/spire-server bundle list -format spiffe
