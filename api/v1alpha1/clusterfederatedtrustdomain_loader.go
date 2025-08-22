3d19604bd40a603abe8e4ca66fa1893989de2ee4
Arian van Putten <arian.vanputten@gmail.com>
2025-08-22 17:58:45 +0200

    Fix static loading (#561)
    * Update clusterstaticentry_loader.go
    
    Hopefully fixes #560 
    
    Signed-off-by: Arian van Putten <arian.vanputten@gmail.com>
    
    * Update clusterfederatedtrustdomain_loader.go
    
    Signed-off-by: Arian van Putten <arian.vanputten@gmail.com>
    
    * Apply suggestions from code review
    
    Signed-off-by: kfox1111 <Kevin.Fox@pnnl.gov>
    
    ---------
    
    Signed-off-by: Arian van Putten <arian.vanputten@gmail.com>
    Signed-off-by: kfox1111 <Kevin.Fox@pnnl.gov>
    Co-authored-by: kfox1111 <Kevin.Fox@pnnl.gov>

--- api/v1alpha1/clusterfederatedtrustdomain_loader.go
+++ api/v1alpha1/clusterfederatedtrustdomain_loader.go
@@ -46,13 +46,12 @@ func ListClusterFederatedTrustDomains(_ context.Context, manifestPath string, ex
 		}
 		fullfile := path.Join(manifestPath, file.Name())
 		entry, err := LoadClusterFederatedTrustDomainFile(fullfile, scheme, expandEnv)
+		if err != nil {
+			return nil, err
+		}
 		// Ignore files of the wrong type in manifestPath
 		if entry.APIVersion != "spire.spiffe.io/v1alpha1" || entry.Kind != "ClusterFederatedTrustDomain" {
 			continue
-		}
-		// Right file type, but error loading
-		if err != nil {
-			return nil, err
 		}
 		res = append(res, *entry)
 	}
